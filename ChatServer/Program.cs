b5163906dc2b380a80250045fe4a4d7a4c42e402
jerometrancart <jerome.trancart@gmail.com>
2023-04-17 18:51:09 +0200

    Base of socket connection realised, server side

---
+++ ChatServer/Program.cs
@@ -0,0 +1,30 @@
+﻿// IMPORT
+using System.Net.Sockets;
+using System.Net;
+
+try
+{
+
+    //SOCKET(TypeIP, TypeSocket, TypeProtocole)
+    Socket socket = new Socket(
+        AddressFamily.InterNetwork,
+        SocketType.Stream,
+        ProtocolType.Tcp);
+
+    //ENDPOINT = IP, PORT
+    IPEndPoint endpoint = new IPEndPoint(
+        IPAddress.Any, 
+        2345
+        );
+
+    socket.Bind(endpoint);
+    //OUVRE LE SOCKET
+    socket.Listen();
+    //BLOQUE LE SOCKET EN OUVERT
+    socket.Accept();
+    Console.WriteLine("Connexion réussie !");
+}
+catch
+{
+    Console.WriteLine("Erreur dans le serveur.");
+}
