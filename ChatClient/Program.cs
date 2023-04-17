ffbeb57ba2c1f39aab92d2c9d21a031103603249
jerometrancart <jerome.trancart@gmail.com>
2023-04-17 23:50:41 +0200

    Chat Client realised, connection to server established, utf8 OK

---
+++ ChatClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+
+//SOCKET(TypeIP, TypeSocket, TypeProtocole)
+var socket = new Socket(
+    AddressFamily.InterNetwork,
+    SocketType.Stream,
+    ProtocolType.Tcp);
+
+//ON MET LOOPBACK POUR L'IP DU LOCALHOST
+var endpoint = new IPEndPoint(
+    IPAddress.Loopback,
+    2345
+);
+
+//SE CONNECTE SUR LE SERVEUR
+socket.Connect(endpoint);
+
+//BOUCLE INFINIE POUR FAIRE TOURNER LE SERVEUR
+while(true)
+{
+    string message = Console.ReadLine();
+    //SI LE MESSAGE EST "Q" SORT DE LA BOUCLE ET DECONNECTE LE SERVEUR
+    if(message == "q")
+    {
+        break;
+    }
+    //INVERSE DU COTE SERVEUR
+    var buffer = Encoding.UTF8.GetBytes(message);
+    socket.Send(buffer);
+
+}
+
+
+    
--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -7,7 +7,7 @@ try
 {
 
     //SOCKET(TypeIP, TypeSocket, TypeProtocole)
-    Socket socket = new Socket(
+    var socket = new Socket(
         AddressFamily.InterNetwork,
         SocketType.Stream,
         ProtocolType.Tcp);
@@ -31,11 +31,16 @@ try
     //CREER UN TABLEAU D'OCTETS QUI RECEVRA LES DONNEES AU MEME FORMAT QUE CE QUI EST ENVOYE
     //CAD byte[]
     //TOUJOURS INITIALISER UN TABLEAU EN PUISSANCE DE 2
-    byte[] buffer = new byte[8];
-    //STOCKER LE RESULTAT DE CE QUI A REELLEMENT ETE LU
-    int nb = clientSocket.Receive(buffer);
-    //AFFICHE LE MESSAGE RECU + SYSTEM.TEXT.DECODE(LE BUFFER, DEPUIS QUEL INDEX, LE NOMBRE D'OCTETS)
-    Console.WriteLine("Message reçu : "+ Encoding.ASCII.GetString(buffer, 0, nb));
+
+    //BOUCLE WHILE INFINIE POUR FAIRE TOURNER LE SERVEUR
+    while(true)
+    {
+        byte[] buffer = new byte[128];
+        //STOCKER LE RESULTAT DE CE QUI A REELLEMENT ETE LU
+        int nb = clientSocket.Receive(buffer);
+        //AFFICHE LE MESSAGE RECU + SYSTEM.TEXT.DECODE(LE BUFFER, DEPUIS QUEL INDEX, LE NOMBRE D'OCTETS)
+        Console.WriteLine("Message reçu : "+ Encoding.UTF8.GetString(buffer, 0, nb));
+    }
 }
 catch
 {
