24e226f658e25f9cd3fa1ed26ac5bf238b1a3aa8
jerometrancart <jerome.trancart@gmail.com>
2023-04-17 19:39:20 +0200

    Connection established, char received and read from telnet client

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,6 +1,7 @@
 ﻿// IMPORT
 using System.Net.Sockets;
 using System.Net;
+using System.Text;
 
 try
 {
@@ -20,9 +21,21 @@ try
     socket.Bind(endpoint);
     //OUVRE LE SOCKET
     socket.Listen();
-    //BLOQUE LE SOCKET EN OUVERT
-    socket.Accept();
+    //CREE UNE NOUVELLE INSTANCE DE SOCKET QUI ATTEND D'ACCEPTER LA CONNEXION
+    var clientSocket = socket.Accept();
     Console.WriteLine("Connexion réussie !");
+    if(clientSocket.RemoteEndPoint is not null)
+    {
+    Console.WriteLine("Client connecté depuis l'adresse : " + clientSocket.RemoteEndPoint.ToString());
+    }
+    //CREER UN TABLEAU D'OCTETS QUI RECEVRA LES DONNEES AU MEME FORMAT QUE CE QUI EST ENVOYE
+    //CAD byte[]
+    //TOUJOURS INITIALISER UN TABLEAU EN PUISSANCE DE 2
+    byte[] buffer = new byte[8];
+    //STOCKER LE RESULTAT DE CE QUI A REELLEMENT ETE LU
+    int nb = clientSocket.Receive(buffer);
+    //AFFICHE LE MESSAGE RECU + SYSTEM.TEXT.DECODE(LE BUFFER, DEPUIS QUEL INDEX, LE NOMBRE D'OCTETS)
+    Console.WriteLine("Message reçu : "+ Encoding.ASCII.GetString(buffer, 0, nb));
 }
 catch
 {
